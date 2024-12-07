212e9d87d8c7ea3743afac9db5725269bbf2e6bb
RAMSI FAVAS KP <57994754+ramsifavaskp@users.noreply.github.com>
2024-12-07 19:12:46 +0530

    Update manage.py

--- manage.py
+++ manage.py
@@ -1,13 +1,17 @@
-                      #!/usr/bin/env python
+#!/usr/bin/env python
 """Django's command-line utility for administrative tasks."""
 import os
 import sys
 
-
 def main():
     """Run administrative tasks."""
+    
+    # Setting the default settings module for Django
+    # You might want to replace 'moviereview.settings' with your own settings file
     os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'moviereview.settings')
+
     try:
+        # Importing Django's execute_from_command_line function
         from django.core.management import execute_from_command_line
     except ImportError as exc:
         raise ImportError(
@@ -15,8 +19,10 @@ def main():
             "available on your PYTHONPATH environment variable? Did you "
             "forget to activate a virtual environment?"
         ) from exc
+    
+    # Adding command-line arguments to execute Django management commands
     execute_from_command_line(sys.argv)
 
-
 if __name__ == '__main__':
+    # Execute the management commands
     main()
